287c6aec85749fd025a14619b145bcfd39c0f716
alamindev <mdalamin6554@gmail.com>
2024-12-16 12:34:01 +0600

    update codes

--- app/not-found.js
+++ app/not-found.js
@@ -18,24 +18,15 @@ and experience warmth like nowhere else.`,
 
 export default function notFound() {
     return (
-    <html lang="en">
-      <body
-        className={`${roboto.variable}   font-roboto`}
-      > 
-        <main className="overflow-x-clip">
-            <div className="flex px-4 flex-col items-center justify-center h-screen bg-gray-100 text-gray-800">
-                <div className="  pb-10">
-                    <SearchImg/>
-                 </div> 
-                <p className="text-lg mb-6  text-center">Oops! The page you're looking for doesn't exist.</p>
-                <Link href="/" 
-                    className="px-6 rounded-full py-3 bg-primary text-white  hover:bg-primary/80 transition">
-                    Go Back Home 
-                </Link>
-            </div>
-        </main>
-      </body>
-    </html>
-
+      <div className="flex px-4 flex-col items-center justify-center h-screen bg-gray-100 text-gray-800">
+      <div className="  pb-10">
+          <SearchImg/>
+       </div> 
+      <p className="text-lg mb-6  text-center">Oops! The page you're looking for doesn't exist.</p>
+      <Link href="/" 
+          className="px-6 rounded-full py-3 bg-primary text-white  hover:bg-primary/80 transition">
+          Go Back Home 
+      </Link>
+  </div> 
   )
 }
